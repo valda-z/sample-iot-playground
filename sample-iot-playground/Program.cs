e3ff47b65f00fded3a445eeac84e344b64c6deae
valda-z <valdemar.zavadsky@microsoft.com>
2017-02-28 13:00:26 +0100

    Add project files.

---
+++ sample-iot-playground/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample_iot_playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
