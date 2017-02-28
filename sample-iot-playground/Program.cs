7646a59307364cb19c1431704e9d18bb22e87f5c
valda-z <valdemar.zavadsky@microsoft.com>
2017-02-28 22:37:19 +0100

    iot emul done

--- sample-iot-playground/Program.cs
+++ sample-iot-playground/Program.cs
@@ -4,12 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Threading;
+
 namespace sample_iot_playground
 {
     class Program
     {
+        static DeviceClient deviceClient;
+        static string iotHubUri = Properties.Settings.Default.IOTHubUri;
+        static string deviceKey = Properties.Settings.Default.DeviceKey;
+        static string deviceName = Properties.Settings.Default.DeviceName;
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(
+                iotHubUri,
+                new DeviceAuthenticationWithRegistrySymmetricKey(deviceName, deviceKey), TransportType.Mqtt);
+
+            SendDeviceToCloudMessagesAsync();
+
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            Random rand = new Random();
+
+            while (true)
+            {
+                var telemetryDataPoint = new
+                {
+                    deviceId = deviceName,
+                    cpuTemperature = Temperature.Temperatures[0].CurrentValue
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(1000).Wait();
+            }
         }
     }
 }
---
+++ sample-iot-playground/Temperature.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace sample_iot_playground
+{
+    class Temperature
+    {
+        public double CurrentValue { get; set; }
+        public string InstanceName { get; set; }
+        public static List<Temperature> Temperatures
+        {
+            get
+            {
+                List<Temperature> result = new List<Temperature>();
+                try
+                {
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        Double temp = Convert.ToDouble(obj["CurrentTemperature"].ToString());
+                        temp = (temp - 2732) / 10.0;
+                        result.Add(new Temperature { CurrentValue = temp, InstanceName = obj["InstanceName"].ToString() });
+                    }
+                }
+                catch (Exception)
+                {
+                    result.Add(new Temperature { CurrentValue = -999, InstanceName = "Error" });
+                }
+                return result;
+            }
+        }
+
+        public static string ToString(List<Temperature> temp)
+        {
+            var sb = new StringBuilder();
+            foreach(var x in temp)
+            {
+                sb.AppendFormat("{0}: {1}", x.InstanceName, x.CurrentValue);
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+}
